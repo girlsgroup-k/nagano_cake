6fa46598d33c6d1d0792e9b33748e8bd87f8d766
riffiy <117071448+riffiy@users.noreply.github.com>
2022-12-17 07:02:39 +0000

    [Add]コントローラー/ルーティング追加

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,19 @@
+class Public::OrdersController < ApplicationController
+  def new
+  end
+
+  def check
+  end
+
+  def create
+  end
+
+  def success
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
