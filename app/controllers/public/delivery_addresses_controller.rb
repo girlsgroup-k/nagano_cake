de63ca5c0e933dc5b8597ee1f4229b96b9301861
riffiy <117071448+riffiy@users.noreply.github.com>
2022-12-17 16:44:37 +0000

    [Add]配送先新規登録フォーム作成

--- app/controllers/public/delivery_addresses_controller.rb
+++ app/controllers/public/delivery_addresses_controller.rb
@@ -1,11 +1,16 @@
 class Public::DeliveryAddressesController < ApplicationController
   def index
+    @delivery_address = DeliveryAddress.new
   end
 
   def edit
   end
 
   def create
+    @delivery_address = DeliveryAddress.new(delivery_address_params)
+    @delivery_address.costomer_id = current_costomer.id
+    @delivery_address.save
+    redirect_to delivery_addresses_path
   end
 
   def update
@@ -13,4 +18,11 @@ class Public::DeliveryAddressesController < ApplicationController
 
   def destroy
   end
+  
+    private
+
+  def delivery_address_params
+    params.require(:delivery_address).permit(:post_code, :address, :name)
+  end
+  
 end
