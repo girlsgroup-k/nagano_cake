c19e368137c03382b684de7dd3b0c45a1745c3e4
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2022-12-17 20:37:19 +0000

    [Add]ジャンル一覧/追加/編集機能を追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,31 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
+    genre = Genre.new(genre_params)
+    genre.save
+    @genres = Genre.all
+    render 'index'
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    @genre = Genre.new
+    @genres = Genre.all
+    redirect_to admin_genres_path
+  end
+  
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
