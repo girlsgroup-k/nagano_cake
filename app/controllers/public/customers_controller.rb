7b28bbbc5b3d9424767862cdb7993f27bb855b34
h-sara <115357536+h-sara@users.noreply.github.com>
2022-12-17 12:56:35 +0000

    [Add]会員登録画面作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,6 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
@@ -12,5 +13,11 @@ class Public::CustomersController < ApplicationController
   end
 
   def withdrawal
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :post_code, :address, :phone_number, :email)
   end
 end
