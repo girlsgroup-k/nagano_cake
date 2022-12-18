0be656f77e7527be90bc9061fd474719ac1ffd8d
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2022-12-18 16:49:06 +0000

    [Fix]画像を扱うgemを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   has_one_attached :item_image
   belongs_to :genre
   has_many :cart_items
+  
+  def get_item_image(width, height)
+    item_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
