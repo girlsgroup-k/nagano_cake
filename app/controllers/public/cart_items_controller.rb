11f923965dbda76f36ba1ca6a102ce5417420cbe
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2022-12-18 15:04:14 +0000

    [Add]カート内商品の数量編集/削除機能追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,9 +4,17 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
+    @cart_items = CartItem.all
+    redirect_to cart_items_path
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    @cart_items = CartItem.all
+    redirect_to cart_items_path
   end
 
   def destroy_all
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   has_one_attached :item_image
   belongs_to :genre
+  has_many :cart_items
 end
