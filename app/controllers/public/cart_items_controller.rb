19bc9842e76ee11f9b9a8e4d5ce70b6bb2dc5815
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2022-12-19 08:46:20 +0000

    [Add]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -25,8 +25,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    cart_item = CartItem.new(cart_item_params)
-    cart_item.save
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_items = CartItem.find_by(customer_id: current_customer.id)
+    if @new_cart_items = Item.find_by(id: @cart_item.item_id)
+      new_quantity = @new_cart_items.itemcart_items.item_quantity.to_i + @cart_item.item_quantity.to_i
+      cart_item.update_attribute(:item_quantity, new_quantity)
+      @cart_item.delete
+    else
+      cart_item.save
+    end
     @cart_items = CartItem.all
     redirect_to cart_items_path
   end
