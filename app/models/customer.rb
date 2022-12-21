725211f9efd4fab6ec9a5ce045abd9e521225705
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2022-12-21 02:50:12 +0000

    [Fix]配送先の選択追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,8 @@ class Customer < ApplicationRecord
   has_many :delivery_addresses, foreign_key: :delivery_address_id, dependent: :destroy
   has_many :cart_items
   has_many :orders
+  
+  def address_display
+    '〒' + post_code + ' ' + address + ' ' + first_name + last_name
+  end
 end
