03d58da1103ade7a670056c121d9886b583ffa66
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2022-12-22 02:02:27 +0000

    [Add]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,13 @@
 class Admin::OrdersController < ApplicationController
   def show
-    @order_detail = OrderDetail.find(params[:id])
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
   end
 
   def update
+    order = Order.find(params[:id])
+    order.update(order_params)
+    @order_detail = OrderDetail.find(params[:id])
+    redirect_to order_path
   end
 end
