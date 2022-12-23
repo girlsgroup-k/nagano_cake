47f357818c9755911a3e5ff0eed0bcc0b9b26366
h-sara <115357536+h-sara@users.noreply.github.com>
2022-12-23 09:19:20 +0000

    [Add] 会員の注文履歴詳細画面のレイアウト調整

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -50,6 +50,8 @@ class Public::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details.all
+    @total = 0
+    @sub_total = 0
   end
 
   private
