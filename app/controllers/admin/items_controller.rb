cb594d8a9bdd5e32aa5df74e03d8060c97d0d6ec
h-sara <115357536+h-sara@users.noreply.github.com>
2022-12-24 04:46:57 +0000

    [Fix] 商品新規登録後の遷移先を変更

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,7 +11,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to admin_items_path
+    redirect_to admin_item_path(@item.id)
   end
 
   def show
