865441087c1bb4c5bfa99de9f4b4b072060c4a07
tttstk518 <tttstk518@gmail.com>
2022-12-24 09:06:40 +0000

    [Fix]注文履歴詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,7 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_details = OrderDetail.where(order_id: @order.id)
+    @total = 0
   end
 
   def update
